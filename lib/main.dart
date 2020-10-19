bf9d7a0334b35ef548f0fe1181ebe6c23680012f
Oğuz KABA <73114283+oguzkaba@users.noreply.github.com>
2020-10-19 16:03:11 +0300

    ilkdegisiklik

--- lib/main.dart
+++ lib/main.dart
@@ -60,7 +60,7 @@ class _MyHomePageState extends State<MyHomePage> {
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter+=2;
+      _counter++;
     });
   }
 
@@ -99,7 +99,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'Buğra ya Tokat At :',
+              'Kaç Gün Oldu :',
             ),
             Text(
               '$_counter',
